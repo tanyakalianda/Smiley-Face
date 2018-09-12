b07a4c83b88a0884302be2fd819da282240ebaa8
tanyakalianda <42977166+tanyakalianda@users.noreply.github.com>
2018-09-12 13:36:53 -0400

    Add files via upload

---
+++ src/SmileyFace.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ * @author Tanya Kalianda
+ * Smiley Face
+ */
+public class SmileyFace 
+{
+
+	public static void main(String[] args) 	{
+		System.out.println("\\\\\\\\\\\\\\");
+		System.out.println("| * * |");
+		System.out.println("|  *  |");
+		System.out.println("| \\_/ |");
+		System.out.println("\\_____/");
+
+	}   
+
+
+}
+ 
